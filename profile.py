7f740b29fbabac41573a7ba97d4d363da717ecb3
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2022-11-05 04:32:54 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -115,7 +115,7 @@ for j in range(params.NICNodeCount):
 
     # Add to lan
     if params.NICNodeCount > 1:
-        iface = node.addInterface("enp175s0")
+        iface = node.addInterface("enp134s0f0")
         lan.addInterface(iface)
         pass
     
