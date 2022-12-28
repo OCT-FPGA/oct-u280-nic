a0767223bc1c09fd5b05c3342b8d3cec8247f628
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2022-12-28 12:42:13 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -40,44 +40,18 @@ for i in range(params.nodeCount):
   # UMass cluster
   host.component_manager_id = "urn:publicid:IDN+cloudlab.umass.edu+authority+cm"
   # Assign to the node hosting the FPGA.
-  #host.component_id = "pc162"
   host.hardware_type = "fpga-alveo-100g"
   # Use the default image for the type of the node selected. 
   host.setUseTypeDefaultImage()
 
-  # Since we want to create network links to the FPGA, it has its own identity.
-  #fpga = request.RawPC("fpga")
-  # UMass cluster
-  #fpga.component_manager_id = "urn:publicid:IDN+cloudlab.umass.edu+authority+cm"
-  # Assign to the fgpa node
-  #fpga.component_id = "fpga-pc162"
-  # Use the default image for the type of the node selected. 
-  #fpga.setUseTypeDefaultImage()
-  # Secret sauce.
-  #fpga.SubNodeOf(host)
-
   #
   # Create lan of all three interfaces.
   #
-  #host_iface1 = host.addInterface()
-  #host_iface1.component_id = "eth2"
-  #host_iface1.addAddress(pg.IPv4Address("192.168.40.3", "255.255.255.0"))
   host_iface = host.addInterface()
   host_iface.component_id = "eth3"
   host_iface.addAddress(pg.IPv4Address("192.168.40." + str(i+10), "255.255.255.0"))
-  #fpga_iface1 = fpga.addInterface()
-  #fpga_iface1.component_id = "eth0"
-  #fpga_iface1.addAddress(pg.IPv4Address("192.168.40.1", "255.255.255.0"))
-  #fpga_iface2 = fpga.addInterface()
-  #fpga_iface2.component_id = "eth1"
-  #fpga_iface2.addAddress(pg.IPv4Address("192.168.40.3", "255.255.255.0"))
 
-  #lan = request.LAN()
-  #lan.addInterface(host_iface1)
   lan.addInterface(host_iface)
-  #lan.addInterface(fpga_iface1)
-  #lan.addInterface(fpga_iface2)
-
 
   # Print the RSpec to the enclosing page.
 
